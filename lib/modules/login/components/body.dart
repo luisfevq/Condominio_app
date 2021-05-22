ae1b10699e4edafd6532d8cc10eaf250eb31dd9f
Nicolas Silva Zuñiga <nsilvazu@ZCO-C02YM00YJG5L.local>
2021-05-21 22:37:31 -0400

    se agregan textos y boton inferior

--- lib/modules/login/components/body.dart
+++ lib/modules/login/components/body.dart
@@ -14,16 +14,93 @@ class Body extends StatelessWidget {
           Positioned(
             child: Image.asset(
               "../assets/images/ws_girl.png",
-              width: size.width * 0.3,
+              width: size.width,
+              height: size.height,
             ),
           ),
           Positioned(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text("CondominApp", style: TextStyle(fontWeight: FontWeight.bold))
-            ],
-          ))
+            top: 30,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text("CondominApp",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white))
+              ],
+            ),
+          ),
+          Positioned(
+            top: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text("Vive en casa",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 36))
+              ],
+            ),
+          ),
+          Positioned(
+            top: 140,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text("Vive Informado.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 36))
+              ],
+            ),
+          ),
+          Positioned(
+            top: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text("Administra e informate de lo que pasa en tu entorno",
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.justify),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            width: size.width * 0.5,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(0),
+                child: Container(
+                  width: 100.0,
+                  height: 70.0,
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "Ingresar",
+                        style: TextStyle(
+                            color: Color.fromRGBO(96, 89, 183, 1),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                      borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(40.0)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
